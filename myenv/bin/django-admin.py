0862bcbdecbcda49d334410b8dac6929e3e14670
Manav Nirmalraj <mnirmalraj@unomaha.edu>
2019-12-11 19:58:37 -0600

    First commit

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Manav/Desktop/MIS/ISQA 8380/Assignments/Assignment 4/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
